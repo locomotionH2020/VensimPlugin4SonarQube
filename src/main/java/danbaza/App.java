863b27dc3ffa4f8bc43f631b118425897b1272ec
danbaza <daniel.bazaco@alumnos.uva.es>
2019-09-21 12:38:39 +0200

    Commit Inicial

---
+++ src/main/java/danbaza/App.java
@@ -0,0 +1,13 @@
+package danbaza;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
