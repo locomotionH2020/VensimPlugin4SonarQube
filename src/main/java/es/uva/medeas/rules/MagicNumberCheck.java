a33df70082a7be88baad2d7acca4b5eac19eb930
danbaza <23432294+DanielBV@users.noreply.github.com>
2020-02-10 16:18:37 +0100

    Actualiazdo descripcion de la regla de los numeros magicos

--- src/main/java/es/uva/medeas/rules/MagicNumberCheck.java
+++ src/main/java/es/uva/medeas/rules/MagicNumberCheck.java
@@ -13,6 +13,7 @@ import org.sonar.api.utils.log.Loggers;
 import org.sonar.check.Rule;
 import org.sonar.check.RuleProperty;
 
+
 @Rule(key = MagicNumberCheck.CHECK_KEY,name=MagicNumberCheck.NAME, description= MagicNumberCheck.HTML_DESCRIPTION)
 public class MagicNumberCheck implements VensimCheck {
     public static final String CHECK_KEY = "magic-number-check";
@@ -21,14 +22,26 @@ public class MagicNumberCheck implements VensimCheck {
             "The number must appear several times to be considered a magic number. The repetitions can be configured as a parameter of the rule.<br><br>" +
             "If the number appears less times than then parameter, it will raise an issue of severity INFO. If it appears more times or exactly as the parameter," +
             " it will raise an issue of severity MAJOR "+
+            "<h2>Exceptions</h2>\n"+
+            "<ul>"+
+            "   <li> Assigning a literal to a variable. Example <code>A = 3~~|</code>   </li>"+
+            "   <li> Literals in tabbed arrays, lookups, one and two dimensional array of values. It's not possible to replace them with constants. </li>"+
+            "   <li> Literals in reality checks </li>"+
+            "   <li> Literals that are compared to switches. Switch variables are variables that begin with 'SWITCH_'. Example: <code>value = IF THEN ELSE(SWITCH_POLICy=1,VALUE1,VALUE2)~~|</code>"+
+            "</ul>"+
+            "<h2> Compound numbers </h2>"+
+            "<p> There are functions that form a 'compound literal' if they are called with literals as arguments. For example SQRT(2) and LOG(3,4) are considered literals, but LOG(3,VALUE) isn't.<br><br>" +
+            "Only the following functions create compound literals: SQRT, TAN, TANH, SIN, SINH, COS, COSH, ARCTAN, ARCCOS, ABS, LN, GAMMA LN, INTEGER, GAME, EXP, POWER, LOG, MODULO, QUANTUM.</p>" +
             "<h2>Noncompliant Code Examples</h2>\n" +
             "<pre>" +
-            "Year policy change energy[scenarios,sectors,final sources]=\n IF THEN ELSE(Choose policies of intensities global or by sector[scenarios]=1, value1, value2)~~|\n" +
+            "VALUE =  IF THEN ELSE(SWITCH_POLICY = 1, 135, 41 )~~|\n" +
             "</pre>\n" +
             "<h2>Compliant Code Examples</h2>\n" +
             "<pre>\n" +
-            "CHOOSE_POLICY = 5~~|\n"+
-            "Year policy change energy[scenarios,sectors,final sources]=\n IF THEN ELSE(Choose policies of intensities global or by sector[scenarios]= CHOOSE_POLICY, value1, value2)~~|\n"+
+            "VALUE_POLICY_ONE = 135~~|\n"+
+            "VALUE_POLICY_TWO = 135~~|\n"+
+            "VALUE = IF THEN ELSE(SWITCH_POLICY = 1, VALUE_POLICY_ONE, VALUE_POLICY_TWO )~~|\n"+
+            "NUMBER = SQRT(2)~~|"+
             "</pre>";
 
     public static final String DEFAULT_REPETITIONS = "3";
