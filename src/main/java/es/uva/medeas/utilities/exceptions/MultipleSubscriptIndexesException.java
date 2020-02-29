3dad220bb04b0e31c2e0eb74def349f4fd9efeb3
danbaza <23432294+DanielBV@users.noreply.github.com>
2020-02-29 12:29:23 +0100

    Añadido SymbolIndexesValidator

---
+++ src/main/java/es/uva/medeas/utilities/exceptions/MultipleSubscriptIndexesException.java
@@ -0,0 +1,15 @@
+package es.uva.medeas.utilities.exceptions;
+
+
+/**
+ * Thrown to indicate that the symbol is indexed with two or more different subscripts (not subscript values)
+ * An example file that would raise this exception is: <br>
+ * <pre>
+ * COUNTRIES: USA, SPAIN, ITALY ~~|
+ * ENERGY_SOURCE: COAL, SOLAR ~~|
+ * var[COUNTRIES] = 3 ~~|
+ * var[ENERGY_SOURCE] = 3 ~~|
+ * </pre>
+ */
+public class MultipleSubscriptIndexesException extends RuntimeException {
+}
---
+++ src/main/java/es/uva/medeas/utilities/exceptions/UnexpectedIndexTypeException.java
@@ -0,0 +1,7 @@
+package es.uva.medeas.utilities.exceptions;
+
+/**
+ * Thrown to indicate that the symbol contains an index that isn't a SUBSCRIPT_NAME or a SUBSCRIPT_VALUE
+ */
+public class UnexpectedIndexTypeException extends RuntimeException{
+}
