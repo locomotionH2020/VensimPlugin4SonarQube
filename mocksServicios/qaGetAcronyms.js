f6000cbf9b2550a38f251f2c0d8e6f04513a1924
herruzo99 <juanherruzo@gmail.com>
2020-11-15 20:57:53 +0100

    Added acronyms dictionary mock

---
+++ mocksServicios/qaGetAcronyms.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        path: '/mocksBackend/qaGetAcronyms',
+        method: 'POST',
+        template: [
+            {
+                id: 1,
+                name: "RES",
+                definition: "Renewable Energy Source"
+            }, {
+                id: 2,
+                name: "2GEN",
+                definition: "Second generation biofuels"
+            }]
+    }
+]
