32af06569b36bbf37fda0a4e9ee687b94feb110e
herruzo99 <juanherruzo@gmail.com>
2020-11-26 16:58:54 +0100

    Added magicNumber to the module filter

--- src/main/java/es/uva/locomotion/parser/visitors/MagicNumberTableVisitor.java
+++ src/main/java/es/uva/locomotion/parser/visitors/MagicNumberTableVisitor.java
@@ -3,81 +3,107 @@ package es.uva.locomotion.parser.visitors;
 import es.uva.locomotion.model.Symbol;
 import es.uva.locomotion.model.SymbolTable;
 import es.uva.locomotion.parser.*;
+import es.uva.locomotion.utilities.logs.LoggingLevel;
+import es.uva.locomotion.utilities.logs.VensimLogger;
 
 
 import static es.uva.locomotion.utilities.UtilityFunctions.stringToFloat;
 import static es.uva.locomotion.utilities.UtilityFunctions.stringToInt;
 
-public class MagicNumberTableVisitor  extends ModelBaseVisitor<Void> {
+public class MagicNumberTableVisitor extends ModelBaseVisitor<Void> {
 
+    protected static VensimLogger LOG = VensimLogger.getInstance();
 
     private SymbolTable numberTable;
 
+    private SymbolTable symbols;
+    private boolean isSymbolFiltered;
 
-
-    public MagicNumberTableVisitor(){
+    public MagicNumberTableVisitor() {
         numberTable = new SymbolTable();
+        isSymbolFiltered = false;
     }
 
+    public void setSymbols(SymbolTable symbols) {
+        this.symbols = symbols;
+    }
+    //getSymbolOrCreate(table,ctx.Id().getText())
 
+    @Override
+    public Void visitLhs(Model.LhsContext ctx) {
 
+        if (symbols == null) {
+            LOG.unique("Symbol table unasigned in MagicNumberVisitor", LoggingLevel.INFO);
+            return null;
+        }
 
-    public  SymbolTable getSymbolTable(Model.FileContext context){
+        if (!symbols.hasSymbol(ctx.Id().getText())) {
+            LOG.error("Found symbol \"" + ctx.Id().getText() + "\" that is not in the symbol table");
+            return null;
+        }
+        Symbol symbol = symbols.getSymbol(ctx.Id().getText());
+        isSymbolFiltered = symbol.isFiltered();
+
+        return null;
+
+    }
+
+    public SymbolTable getSymbolTable(Model.FileContext context) {
         numberTable = new SymbolTable();
         visit(context);
         return numberTable;
     }
 
 
-    private boolean exprIsAConstant(Model.ExprContext ctx){
-        return  ctx.getClass() == Model.ConstContext.class;
+    private boolean exprIsAConstant(Model.ExprContext ctx) {
+        return ctx.getClass() == Model.ConstContext.class;
     }
 
-    private boolean exprIsACompoundNumber(Model.ExprContext ctx){
-        if(ctx.getClass()!= Model.SignExprContext.class)
+    private boolean exprIsACompoundNumber(Model.ExprContext ctx) {
+        if (ctx.getClass() != Model.SignExprContext.class)
             return false;
 
 
         Model.SignExprContext node = (Model.SignExprContext) ctx;
-        if( node.exprAllowSign().getClass() != Model.CallExprContext.class)
+        if (node.exprAllowSign().getClass() != Model.CallExprContext.class)
             return false;
 
 
-       Model.CallExprContext callNode = (Model.CallExprContext) node.exprAllowSign();
-        
-       return isCompoundNumber(callNode.call());
+        Model.CallExprContext callNode = (Model.CallExprContext) node.exprAllowSign();
+
+        return isCompoundNumber(callNode.call());
     }
 
-    private boolean exprIsANumber(Model.ExprContext ctx){
+    private boolean exprIsANumber(Model.ExprContext ctx) {
         return exprIsAConstant(ctx) || exprIsACompoundNumber(ctx);
     }
 
-    private Symbol getSymbolOrCreate(SymbolTable table, String token){
-        if(table.hasSymbol(token))
+    private Symbol getSymbolOrCreate(SymbolTable table, String token) {
+        if (table.hasSymbol(token))
             return table.getSymbol(token);
 
-        else{
+        else {
             return table.addSymbol(new Symbol(token));
         }
     }
+
     @Override
     public Void visitEquation(Model.EquationContext ctx) {
-
-        if(ctx.expr()==null || exprIsANumber(ctx.expr()))
+        if (ctx.expr() == null || exprIsANumber(ctx.expr()))
             return null;
 
 
-        return super.visit(ctx.expr());
+        return super.visitChildren(ctx);
 
     }
 
     @Override
     public Void visitDataEquation(Model.DataEquationContext ctx) {
 
-        if(ctx.expr()==null || exprIsANumber(ctx.expr()))
+        if (ctx.expr() == null || exprIsANumber(ctx.expr()))
             return null;
 
-            return super.visit(ctx.expr());
+        return super.visitChildren(ctx);
 
 
     }
@@ -93,14 +119,14 @@ public class MagicNumberTableVisitor  extends ModelBaseVisitor<Void> {
     }
 
 
-
-
-
     @Override
     public Void visitIntegerConst(Model.IntegerConstContext ctx) {
-        String value = String.valueOf(stringToInt(ctx.getText()));
-        Symbol integer = getSymbolOrCreate(numberTable,value);
-        integer.addDefinitionLine(ctx.start.getLine());
+
+        if (!isSymbolFiltered) {
+            String value = String.valueOf(stringToInt(ctx.getText()));
+            Symbol integer = getSymbolOrCreate(numberTable, value);
+            integer.addDefinitionLine(ctx.start.getLine());
+        }
         return null;
     }
 
@@ -109,15 +135,16 @@ public class MagicNumberTableVisitor  extends ModelBaseVisitor<Void> {
     public Void visitFloatingConst(Model.FloatingConstContext ctx) {
         float value = stringToFloat(ctx.getText());
         String strValue;
-        if(isInteger(value)){
+        if (isInteger(value)) {
             int intValue = (int) value;
             strValue = String.valueOf(intValue);
-        }else{
+        } else {
             strValue = String.valueOf(value);
         }
-
-        Symbol floatSymbol = getSymbolOrCreate(numberTable,strValue);
-        floatSymbol.addDefinitionLine(ctx.start.getLine());
+        if (!isSymbolFiltered) {
+            Symbol floatSymbol = getSymbolOrCreate(numberTable, strValue);
+            floatSymbol.addDefinitionLine(ctx.start.getLine());
+        }
         return null;
     }
 
@@ -131,18 +158,17 @@ public class MagicNumberTableVisitor  extends ModelBaseVisitor<Void> {
     }
 
 
-    private String getFunctionName(Model.CallContext ctx){
+    private String getFunctionName(Model.CallContext ctx) {
         return ctx.Id().getText();
     }
 
 
-    private boolean isCompoundNumber(Model.CallContext ctx){
+    private boolean isCompoundNumber(Model.CallContext ctx) {
         CompoundMagicNumberVisitor visitor = new CompoundMagicNumberVisitor();
         return visitor.visitCall(ctx);
 
 
     }
-
 
 
     @Override
@@ -154,14 +180,14 @@ public class MagicNumberTableVisitor  extends ModelBaseVisitor<Void> {
         }
 
 
-        if(isCompoundNumber(ctx)){
-            Symbol integer = getSymbolOrCreate(numberTable,ctx.getText().trim());
+        if (isCompoundNumber(ctx) && !isSymbolFiltered) {
+            Symbol integer = getSymbolOrCreate(numberTable, ctx.getText().trim());
             integer.addDefinitionLine(ctx.start.getLine());
             return null;
         }
 
 
-        return super.visitCall(ctx);
+        return super.visitChildren(ctx);
     }
 
     @Override
@@ -182,8 +208,8 @@ public class MagicNumberTableVisitor  extends ModelBaseVisitor<Void> {
 
     @Override
     public Void visitLookupDefinition(Model.LookupDefinitionContext ctx) {
-        if(ctx.call()!=null)
-            super.visitCall(ctx.call());
+        if (ctx.call() != null)
+            super.visitChildren(ctx);
         return null;
     }
 
@@ -193,17 +219,17 @@ public class MagicNumberTableVisitor  extends ModelBaseVisitor<Void> {
     }
 
 
-    private boolean isASwitch(String symbol){
+    private boolean isASwitch(String symbol) {
         return symbol.matches("SWITCH_.+");
     }
 
     @Override
     public Void visitExprOperation(Model.ExprOperationContext ctx) {
-        if(ctx.op.getType() == Tokens.Equal)
-            if(isASwitch(ctx.expr(0).getText()) || isASwitch(ctx.expr(1).getText()) )
-              return null;
+        if (ctx.op.getType() == Tokens.Equal)
+            if (isASwitch(ctx.expr(0).getText()) || isASwitch(ctx.expr(1).getText()))
+                return null;
 
-        return super.visitExprOperation(ctx);
+        return super.visitChildren(ctx);
 
     }
 }
