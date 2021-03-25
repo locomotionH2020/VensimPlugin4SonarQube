755cc01762dd3c7de3513016f0641173023991d4
juanh <vcb>
2021-03-25 09:47:18 +0100

    Commented sonar scanner properties

--- src/main/java/es/uva/locomotion/utilities/Constants.java
+++ src/main/java/es/uva/locomotion/utilities/Constants.java
@@ -19,11 +19,11 @@ public class Constants {
 
     public static  final List<String> IGNORED_MAGIC_NUMBERS = Arrays.asList("0","1","100","-1");
 
-    public static final String DICTIONARY_SERVICE_PARAMETER = "vensim.dictionaryService";
-    public static final String DICTIONARY_USERNAME_PARAMETER =  "vensim.dictionaryUsername";
-    public static final String DICTIONARY_PASSWORD_PARAMETER = "vensim.dictionaryPassword";
-    public static final String DICTIONARY_LOG_SERVER_COMMUNICATIONS = "vensim.logServerMessages";
-    public static final String LOG_IN_FILE = "vensim.logFile";
+    public static final String DICTIONARY_SERVICE_PARAMETER = "vensim.dictionary.service";
+    public static final String DICTIONARY_USERNAME_PARAMETER =  "vensim.dictionary.username";
+    public static final String DICTIONARY_PASSWORD_PARAMETER = "vensim.dictionary.password";
+    public static final String DICTIONARY_LOG_SERVER_COMMUNICATIONS = "vensim.log.serverMessages";
+    public static final String LOG_IN_FILE = "vensim.log.file";
     public static final String VIEW_PREFIX = "vensim.view.prefix";
     public static final String MODULE_NAME = "vensim.view.module.name";
     public static final String MODULE_SEPARATOR = "vensim.view.module.separator";
